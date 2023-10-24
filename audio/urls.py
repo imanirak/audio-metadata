964c6f7f3eff61000153b853d02a0dc8eb6cd91e
Karina Garces ✨ <karinadgarces@gmail.com>
2023-10-23 22:58:07 -0400

    form and data available on homepage

--- audio/urls.py
+++ audio/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 from . import views
-from .views import success_message
 
 urlpatterns = [
-     #path("", views.index, name="index"),
-     path('', views.AudioFileUploadView.as_view(), name='audiofile_upload'),
-     path('upload/success/', views.success_message, name='upload_success'),
-    
-   
+    path('', views.index, name='index'),
+    path('upload/', views.AudioFileUploadView.as_view(), name='audiofile_upload'),
+    path('list/', views.AudioFileListView.as_view(), name='audiofile_list'),
+
 ]
